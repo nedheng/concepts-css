43b20f22f42d00c2a42aea7db85d89de69f3da67
Nedhen <nedheng.com>
2024-03-25 18:07:28 +0530

    Tailwind installed

--- src/App.jsx
+++ src/App.jsx
@@ -4,32 +4,31 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  return <div>
+    <div style={{display: 'flex', justifyContent: 'space-around'}}>
+      <div style={{backgroundColor: 'blue'}}>hello</div>
+      <div style={{backgroundColor: 'yellow'}}>hello</div>
+      <div style={{backgroundColor: 'green'}}>hello</div>
+    </div>
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className='flex justify-evenly'>
+      <div className='bg-red-500 '>hello</div>
+      <div className='bg-blue-500 '>hello</div>
+      <div className='bg-yellow-500 '>hello</div>
+    </div>
+
+
+    <div className='grid grid-cols-1 md:grid-cols-3'>
+      <div className='bg-red-500 md:bg-green-500 rounded-md'>hello</div>
+      <div className='bg-blue-500 text-yellow-500 rounded-lg'>hello</div>
+      <div className='bg-yellow-500 text-xl rounded-full'>hello</div>
+    </div>
+
+  </div>
+    
+      
+  
 }
 
 export default App
